23810c9dd87b007f914a24ecdda3c8066d5763cd
flavall318 <75500314+flavall318@users.noreply.github.com>
2020-12-04 13:33:42 -0600

    Add files via upload

---
+++ GitHub practice.py
@@ -0,0 +1,14 @@
+def student1Function():
+    pass
+
+def student2Function():
+    pass
+
+def studentNFunction():
+    pass
+
+######Main Program##########
+
+student1Function()
+student2Function()
+studentNFunction()
